cdb6b46e568dc6627b1cd280f575e15cd3cc9d20
tariktesfa <tariktesfa9090@gmail.com>
2022-01-25 10:37:27 +0300

    Create leaderboard class

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-import _ from 'lodash';
+import Leaderboard from './modules/class.js';
 import './style.css';
+
+(() => {
+  Leaderboard.load();
+})();
+
+document.querySelector('#submit-data').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const score = new Leaderboard(document.querySelector('#fullname').value, document.querySelector('#score').value);
+  Leaderboard.add(score);
+  document.querySelector('#fullname').value = '';
+  document.querySelector('#score').value = '';
+});
+
+document.querySelector('.form').addEventListener('click', () => {
+  Leaderboard.load();
+});
---
+++ src/modules/class.js
@@ -0,0 +1,49 @@
+class Leaderboard {
+  constructor(playerName, score) {
+    this.playerName = playerName;
+    this.score = score;
+  }
+
+  static list = document.querySelector('.scores-list');
+
+  static data = (scoreObj) => {
+    if (scoreObj) {
+      window.localStorage.setItem('scores', JSON.stringify(scoreObj));
+      return true;
+    }
+    const datas = ((window.localStorage.getItem('scores') !== null) ? JSON.parse(window.localStorage.getItem('scores')) : []);
+     return datas;
+  }
+
+  static add = (scoreData) => {
+    if (scoreData.user !== '') {
+      if (this.data().length === 0) {
+        document.querySelector('.no-score').remove();
+      }
+      const store = Leaderboard.data();
+      store.push(scoreData);
+      this.data(store);
+      this.load();
+    }
+  }
+
+  static load = () => {
+    this.list.innerHTML = '';
+    if (this.data().length) {
+      this.data().forEach((result) => {
+        this.append(result);
+      });
+    } else {
+      this.list.innerHTML += `
+        <li class="no-score">No Data to Display</li>
+      `;
+    }
+  }
+
+  static append = (result) => {
+    this.list.innerHTML += `
+      <li>${result.playerName} :  ${result.score}</span></li>
+    `;
+  }
+}
+export default Leaderboard;
