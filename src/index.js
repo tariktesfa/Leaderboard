592b0b2b15cedf470591518587ccd6c706c503ad
tariktesfa <tariktesfa9090@gmail.com>
2022-01-27 10:47:49 +0300

    Remove unneccessary lines

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import Leaderboard from './modules/class.js';
 import './style.css';
 
-(() => {
-  Leaderboard.load();
-})();
+Leaderboard.load();
+
+const name = document.querySelector('#fullname');
+const playerScore = document.querySelector('#score');
 
 document.querySelector('#submit-data').addEventListener('submit', (e) => {
   e.preventDefault();
-  const score = new Leaderboard(document.querySelector('#fullname').value, document.querySelector('#score').value);
+  const score = new Leaderboard(name.value, playerScore.value);
   Leaderboard.add(score);
   document.querySelector('#fullname').value = '';
   document.querySelector('#score').value = '';
